b4d3e9e33b86c37ffceb73cab7733c591b6def8c
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-14 19:40:42 +0100

    Skeleton for score updating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,32 @@ export class AppComponent {
   title = 'Roshambo: The Rock Paper Scissors Game';
 
   scores = [0 , 0]; 
-  playerWeapon = "";
-  computerWeapon = 0;
-  playerWinsRound = -1;
-  enemyWinsRound  = -1;
+  playerWeapon = 0;
+  computerWeapon = 0; 
+  result = 0;
   weapons = [
     'rock',
     'paper',
     'scissors'
   ];  
 
-  pick( weapon: string): void { 
+  calculateResult(){
+    if( this.playerWeapon == this.computerWeapon){
+      //DRAW
+      this.result = 0;
+    }else if(1==1){
+      //PLAYER WIN
+      this.scores[0] += 1; 
+      this.result = 1;
+    }else{
+      //COMPUTER WIN
+      this.scores[1] += 1; 
+      this.result = 2;
+    }
+  }
+
+  pick( weapon: number): void { 
     this.playerWeapon = weapon; 
     this.computerWeapon =  Math.floor(Math.random() * 3 );
-
-    
   }
 }
